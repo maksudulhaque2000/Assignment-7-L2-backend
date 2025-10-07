0a753aaff720e6f78229677a2c8477120cfbe34f
Maksudul-Haque <smmaksudulhaque2000@gmail.com>
2025-10-07 14:38:16 +0600

    Added models

---
+++ src/models/projectModel.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const projectSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  features: [{ type: String }],
+  imageUrl: { type: String, required: true },
+  liveUrl: { type: String, required: true },
+  githubUrl: { type: String, required: true },
+  technologies: [{ type: String }],
+  
+  category: { 
+    type: String, 
+    required: true,
+    enum: ['frontend', 'backend', 'fullstack', 'mobile']
+  },
+
+}, { timestamps: true });
+
+const Project = mongoose.model('Project', projectSchema);
+export default Project;

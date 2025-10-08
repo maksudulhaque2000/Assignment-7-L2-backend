3f072893a9187af1ec56a6c5caa549ebb753d675
Maksudul-Haque <smmaksudulhaque2000@gmail.com>
2025-10-08 12:06:47 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,18 @@ import blogRoutes from './routes/blogRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-async function main() {
-  await connectDB();
+app.use(cors());
+app.use(express.json());
 
-  app.use(cors());
-  app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/blogs', blogRoutes);
+app.use('/api/projects', projectRoutes);
 
-  app.use('/api/auth', authRoutes);
-  app.use('/api/blogs', blogRoutes);
-  app.use('/api/projects', projectRoutes);
-
-  if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Server is running locally on port ${PORT}`);
-    });
-  }
-}
-
-main();
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
